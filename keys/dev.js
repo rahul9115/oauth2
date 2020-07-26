1d158326c3a6e4da658e117b4332ad06714a9eb6
unknown <sudharahul2010@gmail.com>
2020-07-26 15:06:57 +0530

    modified

---
+++ keys/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    clientID: "816997721508-ajn52s3cjj3kn04t3qlodt6ii56i3uoi.apps.googleusercontent.com",
+    clientSecret: "LL1EPMAi7yfddZIFDeXjzW3tD",
+    callbackURL: "/auth/google/callback",
+    cookiekey: "pp",
+    stripePublishableKey: 'pk_test_51GxcfSFRpHIp8ryKMeLNpMThYcDvwbVCGbVMvEhkXDCiaR70rYnh2kC7dJGTD6rn5WQkeuNehmpOPdNpDuYZgm1800K3y5zOqD',
+    stripeSecretKey: 'sk_test_51GxcfSFRpHIp8ryK0JgbZAWhVh3Dei1jAbIVCEeZ4HbfTuBnlL94ZXfCxx8AWiN3Xqg02XAdP1xrjHxLFcKFIuqK00FNCfR7qt',
+    mongoURI: "mongodb+srv://rahul:rahul@cluster0.rpfjy.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
